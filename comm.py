b2fc4e01b79c6784f2382a6d3d339ff855732044
Marcos Vinicius <80992260+MarcosVinicius2908@users.noreply.github.com>
2023-09-27 19:06:50 -0300

    Mudanças no comm

--- comm.py
+++ comm.py
@@ -33,45 +33,98 @@ class RLCommMqttESQ:
             if (rb['robot_id'] == 1):
                 # Assim que começar a partida o goleiro vai virar na vertical
                 self.__mqtt_client.publish(
-                    topic="UFRBots/transmit_robot", payload=f"({rb['1']},{abs(rb['20'])},{abs(rb['0'])})")
+                    topic="UFRBots/transmit_robot", payload=f"({rb['1']},{abs(rb['20'])},{abs(rb['20'])})")
                 # A cada 4 segundos ele vai para frente ou para trás
                 time.sleep(4)
+            
+
                 while (True):
                     self.__mqtt_client.publish(
-                        topic="UFRBots/transmit_robot", payload=f"({rb['1']},{abs(rb['20'])},{abs(rb['20'])})")
+                        topic="UFRBots/transmit_robot", payload=f"({rb['1']},{abs(rb['20'])},{abs(rb['20'])})") ##Robo pra frente
                     
                     time.sleep(4)
 
                     self.__mqtt_client.publish(
-                        topic="UFRBots/transmit_robot", payload=f"({rb['1']},{abs(rb['-20'])},{abs(rb['-20'])})")
+                        topic="UFRBots/transmit_robot", payload=f"({rb['1']},{abs(rb['-20'])},{abs(rb['-20'])})")##Robo pra trás
+                    
+                    time.sleep(4)
+                    
+                    self.__mqtt_client.publish(
+                        topic="UFRBots/transmit_robot", payload=f"({rb['1']},{abs(rb['20'])},{abs(rb['0'])})")##Robo pra vira pra direita
+                    
+                    time.sleep(1)
+
+                    self.__mqtt_client.publish(
+                        topic="UFRBots/transmit_robot", payload=f"({rb['1']},{abs(rb['20'])},{abs(rb['20'])})") #Robo vai pra frente
+                    
+                    time.sleep(4)
+                    
+                    self.__mqtt_client.publish(
+                        topic="UFRBots/transmit_robot", payload=f"({rb['1']},{abs(rb['-20'])},{abs(rb['-20'])})")##robo vem pra trás
+                    
+                    time.sleep(4)
 
 
-            # Jogador 2 
+
+            # Jogador 2 - considerar ele como robô lado direito mais ofensivo 
             if (rb['robot_id'] == 2):
                 # A cada 4 segundos ele vai para frente ou para trás
                 time.sleep(4)
                 while (True):
                     self.__mqtt_client.publish(
-                        topic="UFRBots/transmit_robot", payload=f"({rb['1']},{abs(rb['20'])},{abs(rb['20'])})")
-                    
+                        topic="UFRBots/transmit_robot", payload=f"({rb['2']},{abs(rb['20'])},{abs(rb['20'])})") #Vai pra frente 
+                                        
                     time.sleep(4)
 
                     self.__mqtt_client.publish(
-                        topic="UFRBots/transmit_robot", payload=f"({rb['1']},{abs(rb['-20'])},{abs(rb['-20'])})")
+                        topic="UFRBots/transmit_robot", payload=f"({rb['2']},{abs(rb['-20'])},{abs(rb['-20'])})")#Vai pra trás
+                    time.sleep(4)
 
+                    self.__mqtt_client.publish(
+                        topic="UFRBots/transmit_robot", payload=f"({rb['2']},{abs(rb['0'])},{abs(rb['20'])})")#Vai pra esquerda
+                    time.sleep(2)
+
+                    
+                    self.__mqtt_client.publish(
+                        topic="UFRBots/transmit_robot", payload=f"({rb['2']},{abs(rb['20'])},{abs(rb['0'])})")#Vai pra direita
+                    time.sleep(2)
+
+
+
+                    
             
-            # Jogador 3
+            # Jogador 3 - Mais defensivo (lado esquerdo)
             if (rb['robot_id'] == 3):
                 # A cada 4 segundos ele vai para frente ou para trás
                 time.sleep(4)
                 while (True):
                     self.__mqtt_client.publish(
-                        topic="UFRBots/transmit_robot", payload=f"({rb['1']},{abs(rb['20'])},{abs(rb['20'])})")
+                        topic="UFRBots/transmit_robot", payload=f"({rb['3']},{abs(rb['15'])},{abs(rb['15'])})") #Frente
                     
                     time.sleep(4)
 
                     self.__mqtt_client.publish(
-                        topic="UFRBots/transmit_robot", payload=f"({rb['1']},{abs(rb['-20'])},{abs(rb['-20'])})")
+                        topic="UFRBots/transmit_robot", payload=f"({rb['3']},{abs(rb['-15'])},{abs(rb['-15'])})") #Tras
+                    
+                    time.sleep(4)
+
+
+                                        
+                    self.__mqtt_client.publish(
+                        topic="UFRBots/transmit_robot", payload=f"({rb['3']},{abs(rb['15'])},{abs(rb['0'])})")#Vai pra direita
+                    time.sleep(2)
+
+
+
+                    self.__mqtt_client.publish(
+                        topic="UFRBots/transmit_robot", payload=f"({rb['3']},{abs(rb['0'])},{abs(rb['15'])})")#Vai pra esquerda
+                    time.sleep(2)
+
+
+
+
+
+
 
 
         # message = message[:-1] + ')'
